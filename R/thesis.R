db8c060bc682610024d331e07d138759f63ac2af
Yoshihiko Kunisato <ykunisato@psy.senshu-u.ac.jp>
2021-01-18 00:37:55 +0000

    jpaRmdに対応させた

--- R/thesis.R
+++ R/thesis.R
@@ -1,19 +1,30 @@
 #' R Markdown output formats Template for thesis at Department of Psychology, Senshu University
 #' Format for thesis at Department of Psychology, Senshu University
 #'
+#' @importFrom rmarkdown render
+#' @importFrom rmarkdown pdf_document
+#' @importFrom jpaRmd jpa_cite
+#' @param Rmd_file file name of R Markdown file
+#' @param Bib_file file name of Bib file
 #' @export
-senshu_thesis <- function(){
+senshu_thesis  <- function(Rmd_file, Bib_file) {
+  jpa_cite(Rmd_file, Bib_file)
+  tmp_rmd <- paste0("tmp_", Rmd_file)
   template_tex_file <- system.file("rmarkdown/templates/thesis_senshu/resources/senshu.tex",
-                             package = 'senshuRmd')
-  format_pdf <- rmarkdown::pdf_document(latex_engine = "xelatex",
-                                        template = template_tex_file,
-                                        keep_tex = TRUE,
-                                        toc = TRUE,
-                                        toc_depth = 3,
-                                        highlight = 'tango')
+                                   package = 'senshuRmd')
+  format_pdf <- pdf_document(
+    latex_engine = "xelatex",
+    template = template_tex_file,
+    keep_tex = TRUE,
+    toc = TRUE,
+    toc_depth = 3,
+    highlight = "tango"
+  )
   format_pdf$inherits <- "pdf_document"
-  format_pdf
+  output_file <- strsplit(Rmd_file, ".Rmd")[[1]]
+  render(tmp_rmd, format_pdf, output_file)
 }
+
 
 #' Set Research Compendium of CCP Lab
 #'
