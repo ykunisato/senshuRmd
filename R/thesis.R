ded959a7cf6d63f687a8fb597f89c4698df203bf
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2021-04-03 07:44:04 +0000

    update

--- R/thesis.R
+++ R/thesis.R
@@ -26,13 +26,11 @@ senshu_thesis  <- function(Rmd_file, Bib_file) {
 }
 
 #' Set Research Compendium of senshu
-#'
-#' @param file_name file name and directory name of RMarkdown
 #' @return Make directories for Research Compendium of CCP Lab and
 #' R Markdown file for thesis at Department of Psychology, Senshu University
 #' @examples # set_rc()
 #' @export
-set_rc <- function (file_name = "paper"){
+set_rc <- function (){
   path = getwd()
   # make README
   if(!file.exists(file.path(path, "README.md"))){
@@ -41,9 +39,7 @@ set_rc <- function (file_name = "paper"){
   }
 
   # make RMarkdown file and directory
-  if(!file.exists(file.path(path, paste0(file_name,".Rmd")))){
-    rmarkdown::draft(paste0(file_name,".Rmd"), template = "thesis_senshu", package = "senshuRmd", edit = FALSE)
-  }
+  rmarkdown::draft("paper.Rmd", template = "thesis_senshu", package = "senshuRmd", edit = FALSE)
 
   # make analysis directory
   if(!dir.exists(file.path(path, "analysis"))){
@@ -105,41 +101,54 @@ set_rc_ccp <- function (file_name = "paper"){
   if(!dir.exists(file.path(path, "analysis"))){
     dir.create(file.path(path, "analysis"), showWarnings = FALSE)
     file.create(file.path(path, "analysis/README_analysis.md"), showWarnings = FALSE)
-    writeLines("README about analysis", "analysis/README_analysis.md")
+    writeLines("README(analysis)\n\n
+    \u89e3\u6790\u306b\u4f7f\u3046R\u3084Rmd\u30d5\u30a1\u30a4\u30eb\u3092\u3053
+    \u3053\u306b\u7f6e\u304d\u307e\u3059\u3002\n
+    \u30d5\u30a1\u30a4\u30eb\u540d\u306f\u89e3\u6790\u306e\u9806\u756a\u304c\u308f
+    \u304b\u308b\u3088\u3046\u306b\u3064\u3051\u3066\u304f\u3060\u3055\u3044\u3002\n
+    Rmd\u30d5\u30a1\u30a4\u30eb\u306f\u89e3\u6790\u3068\u305d\u306e\u8aac\u660e
+    \u3092\u307e\u3068\u3081\u3084\u3059\u3044\u306e\u3067\uff0c\u3067\u304d\u308b
+    \u3060\u3051Rmd\u30d5\u30a1\u30a4\u30eb\u3092\u4f7f\u3046\u3088\u3046\u306b
+    \u3057\u3066\u304f\u3060\u3055\u3044\u3002", "analysis/README_analysis.md")
   }
 
   # make data directory
   if(!dir.exists(file.path(path, "analysis/data"))){
     dir.create(file.path(path, "analysis/data"), showWarnings = FALSE)
     file.create(file.path(path, "analysis/data/README_data.md"), showWarnings = FALSE)
-    writeLines("README about data", "analysis/data/README_data.md")
+    writeLines("README(data)\n\n
+               ", "analysis/data/README_data.md")
   }
 
   # make function directory
   if(!dir.exists(file.path(path, "analysis/function"))){
     dir.create(file.path(path, "analysis/function"), showWarnings = FALSE)
     file.create(file.path(path, "analysis/function/README_function.md"), showWarnings = FALSE)
-    writeLines("README about function", "analysis/function/README_function.md")
+    writeLines("README(function)\n\n
+               ", "analysis/function/README_function.md")
   }
 
   # make materials directory
   if(!dir.exists(file.path(path, "materials"))){
     dir.create(file.path(path, "materials"), showWarnings = FALSE)
     file.create(file.path(path, "materials/README_materials.md"), showWarnings = FALSE)
-    writeLines("README about materials", "materials/README_materials.md")
+    writeLines("README(materials)\n\n
+               ", "materials/README_materials.md")
   }
 
   # make exercises directory
   if(!dir.exists(file.path(path, "exercises"))){
     dir.create(file.path(path, "exercises"), showWarnings = FALSE)
     file.create(file.path(path, "exercises/README_exercises.md"), showWarnings = FALSE)
-    writeLines("README about exercises", "exercises/README_exercises.md")
+    writeLines("README(exercises)\n\n
+               ", "exercises/README_exercises.md")
   }
 
   # make labnote directory
   if(!dir.exists(file.path(path, "labnote"))){
     dir.create(file.path(path, "labnote"), showWarnings = FALSE)
     file.create(file.path(path, "labnote/README_labnote.md"), showWarnings = FALSE)
-    writeLines("README about labnote", "materials/README_labnote.md")
+    writeLines("README(labnote)\n\n
+               ", "materials/README_labnote.md")
   }
 }
